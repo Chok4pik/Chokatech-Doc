1b71486858beda882066d6eb16995e7dddb9db39
abdoul <abdoul04.bayere@gmail.com>
2024-10-28 00:59:02 +0100

    lien github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Chok4pik/Chokatech-Doc.git',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
